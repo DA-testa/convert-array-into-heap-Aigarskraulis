d22bff176c32f5450170946a5719490182ae7ec4
Aigarskraulis <62945107+Aigarskraulis@users.noreply.github.com>
2023-03-16 20:13:50 +0000

    testt_3_8

--- build_heap.py
+++ build_heap.py
@@ -1,51 +1,46 @@
-def swap(data, i, res):
-    left = 2*i + 1
-    right = 2*i + 2
-    n = len(data) 
-    
-    if left >= n:
-        return
-    
-    x = left
-    if right < n and data[left] > data[right]:
-        x = right
+import math
 
-    if data[i] > data[x]:  
-        res.append([i, x]) 
-        data[i], data[x] = data[x], data[i]
-        swap(data, x, res) 
-
-def build_heap(data):
+def build_heap(input_list):
     swaps = []
-    n = len(data)
+    n = len(input_list)
 
-    for i in range(int((n - 2)/2), -1, -1):
-        swap(data, i, swaps)
+    for i in range(n//2, -1, -1):
+        heapify(input_list, i, n, swaps)
 
     return swaps
 
+def heapify(input_list, i, n, swaps):
+    left = 2*i + 1
+    right = 2*i + 2
+
+    smallest = i
+    if left < n and input_list[left] < input_list[smallest]:
+        smallest = left
+
+    if right < n and input_list[right] < input_list[smallest]:
+        smallest = right
+
+    if smallest != i:
+        input_list[i], input_list[smallest] = input_list[smallest], input_list[i]
+        swaps.append((i, smallest))
+        heapify(input_list, smallest, n, swaps)
+
 def main():
-    mode = input()
+    mode = input().strip()[0]
+    n = 0
+    input_list = []
+    if mode == "F":
+        with open("./tests/" + input().strip()) as file:
+            n = int(file.readline().strip())
+            input_list = list(map(int, file.readline().split()))
+    elif mode == "I":
+        n = int(input().strip())
+        input_list = list(map(int, input().strip().split()))
 
-    if mode.startswith("F"):
-        name = input()
-        if name != "a":
-            with open(f"./tests/{name}") as f:
-                n = int(f.readline())
-                data = list(map(int, f.readline().split()))
-
-    elif mode.startswith("I"):
-        n = int(input()) 
-        data = list(map(int, input().split()))
-
-    else:
-        return
-    
-    assert len(data) == n
-
-    swaps = build_heap(data) 
-    assert len(swaps) < 4*len(data)
-
+    swaps = build_heap(input_list)
     print(len(swaps))
-    for i, j in swaps: 
-        print(i, j)
+    for swap in swaps:
+        print(swap[0], swap[1])
+
+if __name__ == "__main__":
+    main()
